154b07a72532fa2887b663a562da757a13dbee9d
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-02-11 15:21:21 -0500

    up and running, creating card

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from "./ImageThumbnail.js";
+
+function HeaderContainer(){
+    return(
+        <ImageThumbnail />
+    );
+}
+
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle(){
+    return (
+        <div className="header-top">
+            <h2>Lambda School</h2>
+            <p>@LambdaSchool</p>
+            <p>11 Feb</p>
+        </div>
+    );
+}
+
+export default HeaderTitle;
